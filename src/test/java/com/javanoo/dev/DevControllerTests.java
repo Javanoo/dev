887841bf593e14f9de2f14abe8ee146a13daf64e
Matthews Offen <matthewsoffen2@gmail.com>
2026-03-26 17:17:08 +0200

    add happy flow method test for devController's projects method under DevController unit test and Rename DevControllerTests to DevControllerUnitTests

--- src/test/java/com/javanoo/dev/DevControllerTests.java
+++
@@ -1,6 +0,0 @@
-package com.javanoo.dev;
-
-
-public class DevControllerTests {
-
-}
--- src/test/java/com/javanoo/dev/DevControllerUnitTests.java
+++ src/test/java/com/javanoo/dev/DevControllerUnitTests.java
@@ -1,29 +1,65 @@
 package com.javanoo.dev;
 
+import java.util.LinkedHashSet;
+import java.util.Optional;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
+
+import org.springframework.ui.Model;
 import org.springframework.util.Assert;
 
+import com.javanoo.dev.model.Project;
 import com.javanoo.dev.service.FetchProjectsService;
 
 @ExtendWith(MockitoExtension.class)
 public class DevControllerUnitTests {
 	
 	@Mock
-	FetchProjectsService fetchProjectsService;
+	private FetchProjectsService fetchProjectsService;
+	
+	@Mock
+	private Model model;
 	
 	@InjectMocks
 	DevController devController;
 	
 	@Test
-	@DisplayName("Testing whether the home method runs correctly in a happyflow")
+	@DisplayName("Testing home method's happyflow")
 	public void homeTest() {
 		
 		String result = devController.home();
-		Assert.isTrue(result.equals("home.html"), "success");
+		assertEquals("home.html", result);
+	}
+	
+	@Test
+	@DisplayName("Testing projects method's happyflow")
+	public void projectsTest() {
+		
+		LinkedHashSet<Project> listOfProjects = new LinkedHashSet<>();
+		
+		//assumptions
+		given(fetchProjectsService.fetchProjects("filterType", "filter")).willReturn(listOfProjects);
+		
+		//call
+		String result = devController.projects("filterType","filter",model);
+		
+		//validate
+		verify(model).addAttribute("projects", listOfProjects);
+		assertEquals("projects.html", result);
+	}
+	
+	@Test
+	@DisplayName("Testing collaborate method's happyflow")
+	public void collaborateTest() {
+		String result = devController.collaborate();
+		Assert.isTrue(result.equals("collaborate.html"), "success");
 	}
 }
