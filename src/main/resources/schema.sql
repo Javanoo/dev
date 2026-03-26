ca307914ae7c86316d213437cd08c28d1108c10d
Matthews Offen <matthewsoffen2@gmail.com>
2026-03-26 22:34:46 +0200

    fix syntax errors

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -16,15 +16,15 @@ USE dev;
 --
 
 CREATE TABLE IF NOT EXISTS projects (
- projectId SMALLINT NOT NULL UNSIGNED AUTO_INCREMENT,
+ projectId SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
  projectName VARCHAR(50) NOT NULL,
  projectDescription TEXT NOT NULL,
- projectType ENUM('frontend', 'backend', 'fullstack');
+ projectType ENUM('frontend', 'backend', 'fullstack') DEFAULT 'fullstack',
  projectGithubLink VARCHAR(100) NOT NULL,
  CONSTRAINT projects_pk PRIMARY KEY (projectId),
  KEY idx_projectName (projectName),
  dateCreatedOn DATETIME DEFAULT CURRENT_TIMESTAMP,
- dateCreatedOn DATETIME DEFAULT CURRENT_TIMESTAMP
+ lastUpdatedOn DATETIME DEFAULT CURRENT_TIMESTAMP
 )ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 
@@ -33,15 +33,15 @@ CREATE TABLE IF NOT EXISTS projects (
 --
 
 CREATE TABLE IF NOT EXISTS projectTools (
- projectToolId SMALLINT NOT NULL UNSIGNED AUTO_INCREMENT,
- projectId SMALLINT NOT NULL UNSIGNED,
+ projectToolId SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
+ projectId SMALLINT UNSIGNED NOT NULL,
  projectToolName VARCHAR(50) NOT NULL,
  CONSTRAINT projectTools_pk PRIMARY KEY (projectToolId),
  CONSTRAINT projectTools_projects_fk FOREIGN KEY (projectId)
  REFERENCES projects (projectId) ON DELETE CASCADE ON UPDATE CASCADE,
  UNIQUE uidx_projectId_projectToolName (projectId,projectToolName),
  dateCreatedOn DATETIME DEFAULT CURRENT_TIMESTAMP,
- dateCreatedOn DATETIME DEFAULT CURRENT_TIMESTAMP
+ lastUpdatedOn DATETIME DEFAULT CURRENT_TIMESTAMP
 )ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 
@@ -50,12 +50,12 @@ CREATE TABLE IF NOT EXISTS projectTools (
 --
 
 CREATE TABLE IF NOT EXISTS collaborators (
- collaboratorId SMALLINT NOT NULL UNSIGNED AUTO_INCREMENT,
+ collaboratorId SMALLINT  UNSIGNED NOT NULL AUTO_INCREMENT,
  collaboratorEmail VARCHAR(100) NOT NULL,
  collaboratorEmailSubject VARCHAR(100) NOT NULL,
  collaboratorMessage TEXT NOT NULL,
  CONSTRAINT collaborators_pk PRIMARY KEY (collaboratorId),
  UNIQUE uidx_collaboratorEmail (collaboratorEmail),
  dateCreatedOn DATETIME DEFAULT CURRENT_TIMESTAMP,
- dateCreatedOn DATETIME DEFAULT CURRENT_TIMESTAMP
+ lastUpdatedOn DATETIME DEFAULT CURRENT_TIMESTAMP
 )ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
