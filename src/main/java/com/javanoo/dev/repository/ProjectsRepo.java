34055822db8e577d1bf8a5f1de0f11c24660a649
= <matthewsoffen2@gmail.com>
2026-04-22 13:57:05 +0200

    remove dummy projects and fetch actual projects from database

--- src/main/java/com/javanoo/dev/repository/ProjectsRepo.java
+++ src/main/java/com/javanoo/dev/repository/ProjectsRepo.java
@@ -2,31 +2,15 @@ package com.javanoo.dev.repository;
 
 import java.util.LinkedHashSet;
 
+import org.springframework.data.jdbc.repository.query.Query;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import com.javanoo.dev.model.Project;
 
 @Repository
-public class ProjectsRepo {
-	
-	ProjectsRepo(){}
-	
-	public LinkedHashSet<Project> getProjects(String filterType, String filter){
-		return fetch( filterType, filter);
-	}
-	
-	//dummy routines for testing, needs changing!!
-	protected LinkedHashSet<Project> fetch(String filterType, String filter){
-		
-		LinkedHashSet<Project> projects = new LinkedHashSet<>();
-		for(int i = 0; i < 4; i++){
-			projects.add(new Project(i, "dummyProject"+i,
-										"This is a dummy project, for testing.",
-										"backend", 
-										new String[]{"springboot", "spring", "mysql"}));
-		}
-		
-		return projects;
-	
-	}
+public interface ProjectsRepo extends CrudRepository<Project,Integer> {
+
+	@Query("SELECT projectId, projectName, projectDescription, projectType, projectGithubLink FROM projects WHERE projectType = :projectType")
+	LinkedHashSet<Project> fetchProjects(String projectType);
 }
