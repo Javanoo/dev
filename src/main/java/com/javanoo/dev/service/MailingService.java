a21e43859c31672a2fb7518265aa7e94c434ccbf
= <matthewsoffen2@gmail.com>
2026-04-22 13:53:00 +0200

    update Mailing  Service

--- src/main/java/com/javanoo/dev/service/MailingService.java
+++ src/main/java/com/javanoo/dev/service/MailingService.java
@@ -2,7 +2,20 @@ package com.javanoo.dev.service;
 
 import org.springframework.stereotype.Service;
 
+import com.javanoo.dev.model.Email;
+import com.javanoo.dev.repository.EmailsRepo;
+
 @Service
 public class MailingService {
+    
+    private final EmailsRepo emailRepo;
+
+    MailingService (EmailsRepo emailRepo){
+        this.emailRepo = emailRepo;
+    }
+
+    public void storeEmail(Email email){
+        emailRepo.createEmail(email.getEmailAddress(), email.getEmailSubject(), email.getEmailMessage());
+    }
 
 }
